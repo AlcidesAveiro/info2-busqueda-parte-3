505e71e8425e352eb313b13935996bb44a5aa9aa
AlcidesAveiro <aveiroalcides0910@gmail.com>
2024-04-07 13:51:23 -0400

    b - completo

--- searchAgents.py
+++ searchAgents.py
@@ -589,7 +589,8 @@ class ClosestDotSearchAgent(SearchAgent):
         problem = AnyFoodSearchProblem(gameState)
 
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        return search.breadthFirstSearch(problem)
+        #util.raiseNotDefined()
 
 class AnyFoodSearchProblem(PositionSearchProblem):
     """
@@ -625,6 +626,8 @@ class AnyFoodSearchProblem(PositionSearchProblem):
         x,y = state
 
         "*** YOUR CODE HERE ***"
+
+        return self.food[x][y]
         util.raiseNotDefined()
 
 def mazeDistance(point1, point2, gameState):
